6994220fc337d1dcf59d87023b965cfcf6e57e91
Selim Çınar <btk02672@gmail.com>
2024-12-23 20:58:19 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MauiResource
+# MauiResource
---
+++ Resource/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Resource
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
